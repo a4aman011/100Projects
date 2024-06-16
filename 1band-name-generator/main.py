7928eebded48f153b1be23274903f8dc33d75365
Aman Ranjan <a4aman011@outlook.com>
2024-06-16 22:59:38 +0530

    Band Name Generator code uploaded

---
+++ 1band-name-generator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator")
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("what's your pet's name?\n")
+
+print("Your pet name would be "+city_name+" "+pet_name)
