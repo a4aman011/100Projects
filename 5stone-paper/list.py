6b109f43ad585b11887df61ca97862612d4d0cce
Aman Ranjan <a4aman011@outlook.com>
2024-06-25 00:21:18 +0530

    aaded two projects from day 5

---
+++ 5stone-paper/list.py
@@ -0,0 +1,11 @@
+# fruits = ["strawberries", "nectarines", "Apples", "grapes", "peaches", "cherries", "pears"]
+# fruits[-1] = "Melons"
+# fruits.append("lemons")
+# print(fruits)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[0][2])
